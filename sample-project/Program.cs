a7cc3151640683e460c73a3149212ab63a882714
Luis Carlin <luiscarlin@gmail.com>
2019-02-27 05:11:46 -0500

    start playing with dotnet

---
+++ sample-project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sample_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
